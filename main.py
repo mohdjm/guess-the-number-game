b24d53a45485032ac213389c9c21b4f1af3a05a4
Danial J <mohddanialjaafar@gmail.com>
2026-03-16 14:31:06 +0800

    Refactor guess_num function for improved clarity
    Added in check answer and set difficulty functions for improved clarity.

--- main.py
+++ main.py
@@ -1,50 +1,52 @@
 import art
 import random
 
+# Global constants for easy and hard mode
+EASY_LEVEL_LIVES =10
+HARD_LEVEL_LIVES=5
+
+def set_difficulty():
+	game_mode = input("Choose a difficulty. Type 'easy' or 'hard'.").lower()
+	if game_mode=="easy":
+		return EASY_LEVEL_LIVES
+	else:
+		return HARD_LEVEL_LIVES
+
+def check_answer(user_choice, computers_number, lives):
+	if user_choice == computers_number:
+		print(f"You got it! The answer was {computers_number}!")
+		return 0
+	elif user_choice > computers_number:
+		print(f"Too high.")
+		return lives - 1
+	else:
+		print(f"Too low.")
+		return lives -1
+
+
 # Creating the Guess the Number function, guess_num(). Print logo, and welcome statement.
 def guess_num():
 	print(art.logo)
 
-	is_game_over = False
-
-	user_choice=[]
-	computers_number=[]
-
 	print("Welcome to the Number Guessing Game!")
 	print("I'm thinking of a number between 1 and 100.")
+
 	computers_number=random.randint(1,100)
+	lives=set_difficulty()
 
-	game_mode=input("Choose a difficulty. Type 'easy' or 'hard'.").lower()
-
-	if game_mode == "easy":
-		lives=10
+	user_choice = 0
+	while user_choice !=computers_number:
 		print(f"You have {lives} attempts remaining to guess the number.")
+		user_choice = int(input("Make a guess: "))
 
-		while not is_game_over:
-			user_choice = int(input("Make a guess: "))
-			if user_choice == computers_number:
-				print(f"You got it! The answer was {computers_number}!")
-			elif user_choice > computers_number:
-				lives -= 1
-				print(f"Too high.\n Guess again.")
-			elif user_choice< computers_number:
-				lives-=1
-				print(f"Too low.\n Guess again.")
+		# Track turns using our helper function
+		lives = check_answer(user_choice, computers_number, lives)
 
-	else:
-		lives=5
-		print(f"You have {lives} attempts remaining to guess the number.")
-
-		while not is_game_over:
-			user_choice = int(input("Make a guess: "))
-			if user_choice == computers_number:
-				print(f"You got it! The answer was {computers_number}!")
-			elif user_choice > computers_number:
-				lives -= 1
-				print(f"Too high.\n Guess again.")
-			elif user_choice< computers_number:
-				lives-=1
-				print(f"Too low.\n Guess again.")
+		if lives == 0 and user_choice != computers_number:
+			print(f"You've run out of guesses, you lose.\nThe answer was {computers_number}.")
+			return
+		elif user_choice != computers_number:
+			print("Guess again.")
 
 
 guess_num()
